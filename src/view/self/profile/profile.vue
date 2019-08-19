ddde1e358ed3693c7117023616436dcfc58965de
DF <fzcfydcz@163.com>
2019-08-20 01:49:33 +0800

    profile

---
+++ src/view/self/profile/profile.vue
@@ -0,0 +1,92 @@
+<template>
+  <Card>
+    <Row>
+      <Col span="6">
+        <Timeline pending="true">
+          <TimelineItem>
+            <p class="time">1984</p>
+            <p class="content">出生</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">1990</p>
+            <p class="content">入小学</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">1997</p>
+            <p class="content">学习英文</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">1999</p>
+            <p class="content">接触FF8</p>
+            <p class="content">接触月下夜想曲</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2000</p>
+            <p class="content">中考</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2003</p>
+            <p class="content">高考(4xx)</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2004</p>
+            <p class="content">再次高考(571)</p>
+            <p class="content">海军工程大学地方生院</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2006</p>
+            <p class="content">接触诗人沙加</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2008</p>
+            <p class="content">考研(295)</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2009</p>
+            <p class="content">再次考研(355)</p>
+            <p class="content">华中科技大学计算机学院</p>
+            <p class="content">染上烟瘾</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2013</p>
+            <p class="content">认识Sophia</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2014</p>
+            <p class="content">入职电讯盈科</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2016</p>
+            <p class="content">入职润雅</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2018</p>
+            <p class="content">入职CertusNet</p>
+          </TimelineItem>
+          <TimelineItem>
+            <p class="time">2019</p>
+            <p class="content">副业经商-电子烟</p>
+            <p class="content">戒烟</p>
+            <p class="content">学习日文</p>
+          </TimelineItem>
+          <TimelineItem>待续</TimelineItem>
+        </Timeline>
+      </Col>
+      <Col span="18">
+        <P>
+          测试文本段
+        </P>
+      </Col>
+    </Row>
+  </Card>
+</template>
+
+<style scoped>
+  .time{
+    font-size: 14px;
+    font-weight: bold;
+  }
+  .content{
+    padding-left: 5px;
+  }
+</style>
