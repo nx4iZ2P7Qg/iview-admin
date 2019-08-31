6e216ede49c0805a4254239ecaa49f4a1abfecec
zengf <zengf@certusnet.com.cn>
2019-08-31 17:21:09 +0800

    添加import xxx from '@/xxx'的ctrl + 左键的跳转功能

---
+++ alias.config.js
@@ -0,0 +1,18 @@
+// 需要在idea config中添加webpack configuration file为本文件地址，才可以解决import中的'@/xxx'路径跳转问题
+// 初步分析是idea没有能正常的解析到@
+// 本文件内容与vue.config.js有些类似
+
+// alias.config.js
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@': resolve('src'),
+      'common': resolve('src/common'),
+      'api': resolve('src/api')
+    }
+  }
+}
