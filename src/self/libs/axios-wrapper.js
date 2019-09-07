818857a098a759698f27c215e18f578a313bb1d5
zengf <zengf@certusnet.com.cn>
2019-09-07 16:26:54 +0800

    非跨域访问第三方url

--- src/self/libs/axios-wrapper.js
+++ src/self/libs/axios-wrapper.js
@@ -1,11 +1,19 @@
-// import axios from "@/libs/api.request"
-import axios from 'axios'
+import axios from '@/libs/api.request'
 
 export const get = () => {
-  return axios.get('http://127.0.0.1:9000/test')
-    .then(function (response) {
-      console.log(response)
-    }).catch(function (error) {
-      console.log(error)
-    })
+  // return axios.request({
+  //   'url': 'http://127.0.0.1:9000/test'
+  // }).then(function (response) {
+  //   console.log(response)
+  // }).catch(function (error) {
+  //   console.log(error)
+  // })
+
+  return axios.request({
+    'baseURL': 'http://httpbin.org',
+    'url': '/get'
+  }).then(
+    () => console.log('s'),
+    () => console.log('e')
+  )
 }
